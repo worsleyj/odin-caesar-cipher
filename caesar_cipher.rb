c4f9c0dc31f94d813b0069250e119befce756e0d
worsleyj <josephworsley23@gmail.com>
2023-12-01 12:09:56 -0500

    added uppercase functionality, cipher now remembers case and encodes accordingly

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,17 +5,35 @@
 # SHIFT = gets.chomp
 
 
-STRING = "abcxyz"
+STRING = "ABCXYZ"
 SHIFT = 3
+
+def upcase?(char)
+    if char == char.upcase
+        true
+    else
+        false
+    end
+end
 
 def caesar_cipher(string, shift)
     cipher = string.chars
     cipher.each do |char|
+        upper = upcase?(char) # true if uppercase
         shifted = (char.ord+shift) # shifted char value
+
+        if upper == true # when char was uppercase, adjust to lowercase
+            shifted += 32
+        end
+
         if shifted > 122 
             shifted -= 26
         elsif shifted < 97
             shifted += 26
+        end
+
+        if upper == true # adjust back to uppercase
+            shifted -= 32
         end
         puts shifted.chr
     end
