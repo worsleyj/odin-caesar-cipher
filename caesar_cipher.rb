4570b9e0815c62fbf53da3e30d6bcc2718e60c43
worsleyj <josephworsley23@gmail.com>
2023-12-01 11:56:27 -0500

    lowercase a-z wrapping added

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,24 @@
+# puts "Enter String\n"
+# STRING = gets.chomp
+
+# puts "Enter Shift Value"
+# SHIFT = gets.chomp
+
+
+STRING = "abcxyz"
+SHIFT = 3
+
 def caesar_cipher(string, shift)
     cipher = string.chars
-    cipher.each { |char| puts (char.ord+shift).chr}
+    cipher.each do |char|
+        shifted = (char.ord+shift) # shifted char value
+        if shifted > 122 
+            shifted -= 26
+        elsif shifted < 97
+            shifted += 26
+        end
+        puts shifted.chr
+    end
 end
 
-caesar_cipher("test", 3)
+caesar_cipher(STRING, SHIFT.to_i)
